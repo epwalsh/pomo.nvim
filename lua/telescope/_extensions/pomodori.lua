276d49e0468c42e4b5a2eab4c3f6c1a4e8c5f5c1
epwalsh <petew@allenai.org>
2024-04-02 08:43:11 -0700

    style

--- lua/telescope/_extensions/pomodori.lua
+++ lua/telescope/_extensions/pomodori.lua
@@ -1,12 +1,12 @@
 local pickers = require "telescope.pickers"
 local previewers = require "telescope.previewers"
-local utils = require('telescope.previewers.utils')
+local utils = require "telescope.previewers.utils"
 local finders = require "telescope.finders"
 local conf = require("telescope.config").values
 local actions = require "telescope.actions"
 local action_state = require "telescope.actions.state"
-local pomo = require("pomo")
-local themes = require("telescope.themes")
+local pomo = require "pomo"
+local themes = require "telescope.themes"
 
 local pomodori_timers = nil
 local options = {}
@@ -63,15 +63,13 @@ local stop_timer = function(bufnr)
   if timer == nil then
     return
   end
-  local confirm = vim.fn.input(
-    string.format("Stop timer %s? [y/n]: ", tostring(timer))
-  )
+  local confirm = vim.fn.input(string.format("Stop timer %s? [y/n]: ", tostring(timer)))
   if string.sub(string.lower(confirm), 0, 1) == "y" then
     pomo.stop_timer(timer)
     refresh(bufnr)
     return
   end
-  print("Didn't stop timer")
+  print "Didn't stop timer"
 end
 
 local close = function(bufnr)
@@ -79,19 +77,20 @@ local close = function(bufnr)
 end
 
 pomodori_timers = function(opts)
-  options = opts or themes.get_dropdown({
-    layout_strategy = "horizontal",
-    layout_config = {
-      width = 80,
-      preview_width = 16
+  options = opts
+    or themes.get_dropdown {
+      layout_strategy = "horizontal",
+      layout_config = {
+        width = 80,
+        preview_width = 16,
+      },
     }
-  })
   local timers = {}
-  for _,timer in pairs(pomo.get_all_timers()) do
+  for _, timer in pairs(pomo.get_all_timers()) do
     local key = tostring(timer)
-    local _,_,h = key:find("([%d]+)h")
-    local _,_,m = key:find("([%d]+)m")
-    local _,_,s = key:find("([%d]+)s")
+    local _, _, h = key:find "([%d]+)h"
+    local _, _, m = key:find "([%d]+)m"
+    local _, _, s = key:find "([%d]+)s"
     h = h or "00"
     m = m or "00"
     s = s or "00"
@@ -99,75 +98,79 @@ pomodori_timers = function(opts)
     table.insert(timers, {
       value = timer,
       display = key,
-      ordinal = 1 .. h .. m .. s .. key
+      ordinal = 1 .. h .. m .. s .. key,
     })
   end
 
-  table.sort(timers, function(a,b) return a.ordinal < b.ordinal end)
+  table.sort(timers, function(a, b)
+    return a.ordinal < b.ordinal
+  end)
 
-  pickers.new(options, {
-    prompt_title = "Pomodori Timers",
-    finder = finders.new_table {
-      results = timers,
-      entry_maker = function(entry)
-        return entry
-      end
-    },
-    sorter = conf.generic_sorter(options),
-    previewer =  previewers.new_buffer_previewer({
-      title = "Key Map Info",
-      define_preview = function(self, _, _)
-        local buf = self.state.bufnr
-        utils.highlighter(buf, "markdown")
-        vim.api.nvim_buf_set_option(buf, 'modifiable', true)
-        vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
-        vim.api.nvim_buf_set_lines(buf, 0, -1, true, {
-          "",
-          "   <C-p> Pause",
-          "   <C-r> Resume  ",
-          "",
-          "   <C-h> Hide",
-          "   <C-v> View",
-          "",
-          "   <C-s> Stop",
-          "",
-          "   <Esc> Close",
-          " <Enter> Close"
-        })
-        vim.api.nvim_buf_set_option(buf, 'modifiable', false)
-      end
-    }),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        close(prompt_bufnr)
-      end)
+  pickers
+    .new(options, {
+      prompt_title = "Pomodori Timers",
+      finder = finders.new_table {
+        results = timers,
+        entry_maker = function(entry)
+          return entry
+        end,
+      },
+      sorter = conf.generic_sorter(options),
+      previewer = previewers.new_buffer_previewer {
+        title = "Key Map Info",
+        define_preview = function(self, _, _)
+          local buf = self.state.bufnr
+          utils.highlighter(buf, "markdown")
+          vim.api.nvim_buf_set_option(buf, "modifiable", true)
+          vim.api.nvim_buf_set_option(buf, "buftype", "nofile")
+          vim.api.nvim_buf_set_lines(buf, 0, -1, true, {
+            "",
+            "   <C-p> Pause",
+            "   <C-r> Resume  ",
+            "",
+            "   <C-h> Hide",
+            "   <C-v> View",
+            "",
+            "   <C-s> Stop",
+            "",
+            "   <Esc> Close",
+            " <Enter> Close",
+          })
+          vim.api.nvim_buf_set_option(buf, "modifiable", false)
+        end,
+      },
+      attach_mappings = function(prompt_bufnr, map)
+        actions.select_default:replace(function()
+          close(prompt_bufnr)
+        end)
 
-      map("n", "<C-p>", pause_timer)
-      map("i", "<C-p>", pause_timer)
+        map("n", "<C-p>", pause_timer)
+        map("i", "<C-p>", pause_timer)
 
-      map("n", "<C-r>", resume_timer)
-      map("i", "<C-r>", resume_timer)
+        map("n", "<C-r>", resume_timer)
+        map("i", "<C-r>", resume_timer)
 
-      map("n", "<C-h>", hide_timer)
-      map("i", "<C-h>", hide_timer)
+        map("n", "<C-h>", hide_timer)
+        map("i", "<C-h>", hide_timer)
 
-      map("n", "<C-v>", view_timer)
-      map("i", "<C-v>", view_timer)
+        map("n", "<C-v>", view_timer)
+        map("i", "<C-v>", view_timer)
 
-      map("n", "<C-s>", stop_timer)
-      map("i", "<C-s>", stop_timer)
+        map("n", "<C-s>", stop_timer)
+        map("i", "<C-s>", stop_timer)
 
-      map("n", "<esc>", close)
-      map("i", "<esc>", close)
-      return true
-    end,
-  }):find()
+        map("n", "<esc>", close)
+        map("i", "<esc>", close)
+        return true
+      end,
+    })
+    :find()
 end
 
-return require("telescope").register_extension(
-  {
-    exports = {
-      timers = function() pomodori_timers() end
-    }
-  }
-)
+return require("telescope").register_extension {
+  exports = {
+    timers = function()
+      pomodori_timers()
+    end,
+  },
+}
